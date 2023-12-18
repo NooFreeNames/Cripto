a9d6921b0d987642f84e0a7873323ce074610511
NooFreeNames <89774221+NooFreeNames@users.noreply.github.com>
2023-12-18 19:41:19 +0500

    BlockCipher implements ICipher
    * BlockCipher no longer uses PKCS7
    * The pkcs7Pad, pkcs7Unpad, GenerateIV and isCipherDataValid functions
    have been removed
    * The implementations of the Encrypt and Decrypt functions have been
    changed in accordance with the ICipher interface
    * Added the Salt and BlockSize methods
    * Added saltToIV and NewBlockCipher functions

--- pkg/crypto/cipher/block/blockcipher.go
+++ pkg/crypto/cipher/block/blockcipher.go
@@ -2,93 +2,59 @@
 package block
 
 import (
-	"bytes"
 	"crypto/cipher"
-	"crypto/rand"
-	"errors"
+	"fmt"
 )
 
 // BlockCipher represents a block cipher that can be used for encryption
 // and decryption.
 type BlockCipher struct {
-	block cipher.Block
+	salt        []byte
+	encryptMode cipher.BlockMode
+	decryptMode cipher.BlockMode
 }
 
-// pkcs7Pad pads the given data using PKCS#7 padding.
-func (a BlockCipher) pkcs7Pad(data []byte) []byte {
-	blockSize := a.block.BlockSize()
-	padding := blockSize - len(data)%blockSize
-	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(data, padtext...)
+func (bc BlockCipher) Salt() []byte {
+	return bc.salt
 }
 
-// pkcs7Unpad removes PKCS7 padding from the given byte slice.
-// It returns an error if the padding is invalid.
-func (a BlockCipher) pkcs7Unpad(data []byte) ([]byte, error) {
-	length := len(data)
-	unpadding := int(data[length-1])
-	if unpadding > a.block.BlockSize() || unpadding == 0 {
-		return nil, errors.New("invalid padding")
-	}
-	return data[:(length - unpadding)], nil
+func (bc BlockCipher) Encrypt(dst, src []byte) (err error) {
+	defer func() {
+		if msg := recover(); msg != nil {
+			err = fmt.Errorf("%v", msg)
+		}
+	}()
+	bc.encryptMode.CryptBlocks(dst, src)
+	return nil
 }
 
-// generateIV generates a random initialization vector (IV) for encryption.
-// It returns the generated IV, along with an error if generation fails.
-func (a BlockCipher) generateIV() ([]byte, error) {
-	iv := make([]byte, a.block.BlockSize())
-	if _, err := rand.Read(iv); err != nil {
-		return nil, err
-	}
-	return iv, nil
+func (bc BlockCipher) Decrypt(dst, src []byte) (err error) {
+	defer func() {
+		if msg := recover(); msg != nil {
+			err = fmt.Errorf("%v", msg)
+		}
+	}()
+	bc.decryptMode.CryptBlocks(dst, src) //panic
+	return nil
 }
 
-// isCipherDataValid checks if the given ciphertext is valid for decryption.
-// It returns true if the length of the ciphertext is a multiple of
-// the block size and at least two blocks long.
-func (bc BlockCipher) isCipherDataValid(cipherData []byte) bool {
-	l := len(cipherData)
-	blockSize := bc.block.BlockSize()
-	return l%blockSize == 0 && l >= blockSize*2
+func (bc BlockCipher) BlockSize() int {
+	return bc.encryptMode.BlockSize()
 }
 
-// Encrypt encrypts the given plainData using CBC mode with PKCS#7 padding
-// and returns the encrypted data along with the IV used.
-func (bc BlockCipher) Encrypt(plainData []byte) ([]byte, error) {
-	iv, err := bc.generateIV()
-	if err != nil {
-		return nil, err
-	}
-
-	mode := cipher.NewCBCEncrypter(bc.block, iv)
-	plainData = bc.pkcs7Pad(plainData)
-
-	cipherData := make([]byte, len(plainData))
-	mode.CryptBlocks(cipherData, plainData)
-
-	return append(cipherData, iv...), nil
+// saltToIV creates an IV from the given salt and IV size.
+func saltToIV(salt []byte, IVsize int) []byte {
+	iv := make([]byte, IVsize)
+	copy(iv, salt)
+	return iv
 }
 
-// Decrypt decrypts the given cipherData using CBC mode with PKCS#7 unpadding
-// and returns the decrypted plain data.
-func (bc BlockCipher) Decrypt(cipherData []byte) ([]byte, error) {
-	if !bc.isCipherDataValid(cipherData) {
-		return cipherData, errors.New("invalid cipher data format")
-	}
-
-	ivStartIndex := len(cipherData) - bc.block.BlockSize()
-	iv := cipherData[ivStartIndex:]
-	cipherData = cipherData[:ivStartIndex]
-
-	mode := cipher.NewCBCDecrypter(bc.block, iv)
-
-	plainData := make([]byte, len(cipherData))
-	mode.CryptBlocks(plainData, cipherData)
-
-	plainData, err := bc.pkcs7Unpad(plainData)
-	if err != nil {
-		return cipherData, err
-	}
-
-	return plainData, nil
+// NewBlockCipher creates a new BlockCipher using the provided block and salt.
+func NewBlockCipher(block cipher.Block, salt []byte) (BlockCipher, error) {
+	iv := saltToIV(salt, block.BlockSize())
+	return BlockCipher{
+		salt,
+		cipher.NewCBCEncrypter(block, iv),
+		cipher.NewCBCDecrypter(block, iv),
+	}, nil
 }
