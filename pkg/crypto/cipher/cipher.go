b3cb26c1f3e08cdeaec1038c9fa44670cc46d1ce
NooFreeNames <89774221+NooFreeNames@users.noreply.github.com>
2023-12-18 19:26:05 +0500

    Cipher interface has been changed
    * The prefix I has been added to the interface name
    * Added comments to the Encrypt and Decrypt methods describing their
    behavior and parameters.
    * The parameters of the Encrypt and Decrypt methods have been changed -
    now they accept two slices of dst and src bytes, instead of one slice of
    bytes.
    * The Encrypt and Decrypt methods now return an error.
    * Added the BlockSize method, which returns the block size for a given
    cipher.
    * The Salt method has been added, which returns the salt associated with
    this cipher.

--- pkg/crypto/cipher/cipher.go
+++ pkg/crypto/cipher/cipher.go
@@ -1,8 +1,34 @@
 // Package cipher provides the ability to encrypt and decrypt data.
 package cipher
 
-// Cipher represents a cipher that can be used to encrypt and decrypt data
-type Cipher interface {
-	Encrypt([]byte) ([]byte, error)
-	Decrypt([]byte) ([]byte, error)
+type ICipher interface {
+	// Encrypt encrypts a number of blocks. The length of
+	// src must be a multiple of the block size. Dst and src must overlap
+	// entirely or not at all.
+	//
+	// If len(dst) < len(src), Encrypt return error. It is acceptable
+	// to pass a dst bigger than src, and in that case, Encrypt will
+	// only update dst[:len(src)] and will not touch the rest of dst.
+	//
+	// Multiple calls to Encrypt behave as if the concatenation of
+	// the src buffers was passed in a single run. That is, ICipher
+	// maintains state and does not reset at each Encrypt call.
+	Encrypt(dst, src []byte) error
+	// Decrypt decrypts a number of blocks. The length of
+	// src must be a multiple of the block size. Dst and src must overlap
+	// entirely or not at all.
+	//
+	// If len(dst) < len(src), Decrypt return error. It is acceptable
+	// to pass a dst bigger than src, and in that case, Decrypt will
+	// only update dst[:len(src)] and will not touch the rest of dst.
+	//
+	// Multiple calls to Decrypt behave as if the concatenation of
+	// the src buffers was passed in a single run. That is, ICipher
+	// maintains state and does not reset at each Decrypt call.
+	Decrypt(dst, src []byte) error
+	// BlockSize returns the block size of the cipher.
+	// If the cipher is stream then 1 will be returned.
+	BlockSize() int
+	// Salt returns the salt associated with the cipher.
+	Salt() []byte
 }
