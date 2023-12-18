c6d6ead73cd05f36ad3bcd24c089cda719bb59eb
NooFreeNames <89774221+NooFreeNames@users.noreply.github.com>
2023-12-18 21:11:11 +0500

    Added stream package
    Package stream contains the StreamCipher structure.
    StreamCipher allows you to encrypt and decrypt data using cipher.Stream

---
+++ pkg/crypto/cipher/stream/stream.go
@@ -0,0 +1,48 @@
+// Package stream provides a simple implementation of a stream cipher.
+package stream
+
+import (
+	"crypto/cipher"
+	"fmt"
+)
+
+// StreamCipher represents a stream cipher that can be used for
+// encryption and decryption.
+type StreamCipher struct {
+	salt   []byte
+	stream cipher.Stream
+}
+
+func (sc StreamCipher) Encrypt(dst, src []byte) (err error) {
+	defer func() {
+		if msg := recover(); msg != nil {
+			err = fmt.Errorf("%v", msg)
+		}
+	}()
+	sc.stream.XORKeyStream(dst, src)
+	return nil
+}
+
+func (sc StreamCipher) Decrypt(dst, src []byte) (err error) {
+	defer func() {
+		if msg := recover(); msg != nil {
+			err = fmt.Errorf("%v", msg)
+		}
+	}()
+	sc.stream.XORKeyStream(dst, src)
+	return nil
+}
+
+func (sc StreamCipher) BlockSize() int {
+	return 1
+}
+
+func (sc StreamCipher) Salt() []byte {
+	return sc.salt
+}
+
+// NewStreamCipher creates a new instance of StreamCipher with the given
+// stream and salt.
+func NewStreamCipher(stream cipher.Stream, salt []byte) StreamCipher {
+	return StreamCipher{salt, stream}
+}
